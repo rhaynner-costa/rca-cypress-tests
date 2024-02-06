a299eab3a2c879e7083c29a3aac73a9c278c96fb
Rhaynner Assunção <rhaynner@rcaMac.local>
2024-02-06 17:27:14 -0300

    implementado o relatorio de execução

--- README.md
+++ README.md
@@ -29,4 +29,6 @@ Na raiz do projeto rodar o comando:
 
 Em caso de falha em algum cenário, um vídeo registrando a execução dos testes é salvo no diretório 'cypress/videos'
 
-Em caso de falha em algum cenário, um vídeo que registra a execução dos testes, juntamente com capturas de tela, é salvo no diretório cypress/screenshots
+Em caso de falha em algum cenário, um vídeo que registra a execução dos testes, juntamente com capturas de tela, é salvo no diretório 'cypress/screenshots'
+
+A cada execução, o relatório dos testes será gerado e armazenado no diretório 'rca-cypress-tests/test-report', sendo sempre sobrescrito
--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,24 @@ module.exports = defineConfig({
   viewportWidth: 1280,
   video: true,
   videoCompression: true,
-
+  retries: 1,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'test-report',
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    ignoreVideos: true,
+    videoOnFailOnly: false,
+    quiet: false,
+    timestamp: 'mmddyyyy_HHMMss',
+    overwrite: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
 
     },
